d1cb2dbd12cacc57c2c6c0ae2b5aa38e1279c3bc
Rostyslav Yanchynskyi <yanchinskiy432@gmail.com>
2020-07-14 17:15:15 +0300

    create activity_menu.xml

---
+++ src/main/java/ua/com/android/yanchynskyi/godofmath/MenuActivity.kt
@@ -0,0 +1,12 @@
+package ua.com.android.yanchynskyi.godofmath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
